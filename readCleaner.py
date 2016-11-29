d5d889cad89e1b66f8ec64c1a68ef8f0babfebef
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2016-11-29 09:39:54 -0800

    update on working version

--- readCleaner.py
+++ readCleaner.py
@@ -10,7 +10,7 @@ def trimmomaticCaller(instance):
     this function deals with the trimming of the reads using Trimmomatic. Recommended options, ILLUMINACLIP:path2AdaptersFile:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
     '''
 
-    print('working with file {}',instance)
+    print('working with file {}'.format(instance))
 
     logFile=logFilesDir+instance+'.messagesFromTrimming.txt'
 
@@ -23,13 +23,11 @@ def trimmomaticCaller(instance):
     outputFile2a=cleanReadsDir+instance+'_ALL2.clean.fastq.gz'
     outputFile2b=cleanReadsDir+instance+'_ALL2.garbage.fastq.gz'
 
-    cmd=javaPath+' -jar '+trimmomaticPath+' PE -threads 4 -phred33 -trimlog %s %s %s %s %s %s %s ILLUMINACLIP:%s:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36'%(logFile,inputFile1,inputFile2,outputFile1a,outputFile1b,outputFile2a,outputFile2b,path2Adapter)  
+    cmd='time '+javaPath+' -jar '+trimmomaticPath+' PE -threads 4 -phred33 -trimlog %s %s %s %s %s %s %s ILLUMINACLIP:%s:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36'%(logFile,inputFile1,inputFile2,outputFile1a,outputFile1b,outputFile2a,outputFile2b,path2Adapter)  
 
     print(cmd)
-    #os.system(cmd)
+    os.system(cmd)
     print()
-
-    sys.exit()
     
     return None
 
@@ -38,7 +36,7 @@ def trimmomaticCaller(instance):
 # 0. defining user variables
 rawReadsDir='/Users/alomana/scratch/FASTQ/'
 cleanReadsDir='/Users/alomana/scratch/cleanFASTQ/'
-logFilesDir='/Users/alomana/scratch/timmomaticLogs/'
+logFilesDir='/Users/alomana/scratch/trimmomaticLogs/'
 path2Adapter='/Users/alomana/software/Trimmomatic-0.36/adapters/TruSeq3-PE-2.fa'
 
 javaPath='/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java'
